955ecb8cf43b212b177a4235c7f865bdbb8e41c3
Siddhant Dubey <sid12.dubey@gmail.com>
2020-02-21 11:33:48 -0500

    first commit

---
+++ getoptions.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+from yahoo_fin import options
+
+style.use("ggplot")
+
+chain = options.get_options_chain("SPY")
+
+data = pd.DataFrame(chain['calls'])
+data1 = pd.DataFrame(chain['puts'])
+
+data.to_csv('calls.csv')
+data1.to_csv('puts.csv')
+
+
+plt.show()
+print(chain["calls"].head())
