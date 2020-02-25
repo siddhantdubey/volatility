a4338543de54b03b0b15fc4784e6eab430c81a7c
lollipop1dino <43811294+lollipop1dino@users.noreply.github.com>
2020-02-25 13:49:48 -0500

    lajdfl;adskfj

--- getoptions.py
+++ getoptions.py
@@ -8,43 +8,53 @@ from mpl_toolkits import mplot3d
 from yahoo_fin import options
 
 style.use("ggplot")
-datess = ['03/06/2020','03/13/2020','03/20/2020','03/27/2020','04/03/2020','04/17/2020','05/15/2020','06/19/2020','07/17/2020','08/21/2020','09/18/2020','10/16/2020','01/15/2021','03/19/2021','06/18/2021','09/17/2021','01/21/2022','03/18/2022','06/17/2022']
+stock = "TSLA"
+datess = options.get_expiration_dates(stock)
+# datess = ['03/06/2020','03/13/2020','03/20/2020','03/27/2020','04/03/2020','04/17/2020','05/15/2020','06/19/2020','07/17/2020','08/21/2020','09/18/2020','10/16/2020','01/15/2021','03/19/2021','06/18/2021','09/17/2021','01/21/2022','03/18/2022','06/17/2022']
+days = []
+days1 = []
+volatility = []
+volatility1 = []
+tdata = pd.DataFrame()
+tdata1 = pd.DataFrame()
 for i in range(len(datess)):
     d = datess[i]
-    chain = options.get_options_chain("TSLA", d)
+    chain = options.get_options_chain(stock, d)
 
     data = pd.DataFrame(chain['calls']) 
-
-    expiry = d[6:] + '-' + d[:2] + '-' + d[3:5] #'2020-03-20'
-
-    days = []
+    
     for x in chain['calls']['Last Trade Date']:
-        days.append(int((dt.datetime.strptime(expiry, '%Y-%m-%d') - dt.datetime.today()).days))
+        days.append(int((dt.datetime.strptime(d, '%B %d, %Y') - dt.datetime.today()).days))
 
-    data["Maturity"] = days
     data1 = pd.DataFrame(chain['puts'])
-    days1 = []
+    
     for x in chain['puts']['Last Trade Date']:
-        days1.append(int((dt.datetime.strptime(expiry, '%Y-%m-%d') - dt.datetime.today()).days))
-
-    data1["Maturity"] = days1
-    for x in data['Maturity']:
-        x = int(x)
-
-    for x in data['Strike']:
-        x  = int(x)
-    volatility = []
+        days1.append(int((dt.datetime.strptime(d, '%B %d, %Y') - dt.datetime.today()).days))
     for x in data['Implied Volatility']:
         stringy = x[:-1]
         stringy = stringy.replace(',','')
         x = float(stringy)
         volatility.append(x)
-
-    data['Implied Volatility'] = volatility
+    for x in data1['Implied Volatility']:
+        stringy = x[:-1]
+        stringy = stringy.replace(',','')
+        x = float(stringy)
+        volatility1.append(x)
+    tdata = pd.concat([tdata,data])
+    tdata1 = pd.concat([tdata1,data1])
     # data = data[data['Maturity'] != 0]
-    data.to_csv('Data/calls' + d[6:] + '-' + d[:2] + '-' + d[3:5] + '.csv')
-    data1.to_csv('Data/puts' + d[6:] + '-' + d[:2] + '-' + d[3:5] + '.csv')
+tdata["Maturity"] = days
+tdata1["Maturity"] = days1
+tdata['Implied Volatility'] = volatility
+tdata1['Implied Volatility'] = volatility1
+tdata = tdata[tdata['Implied Volatility'] != 0]
+tdata1 = tdata1[tdata1['Implied Volatility'] != 0]
+# for x in tdata['Maturity']:
+#     x = int(x)
 
+# for x in tdata['Strike']:
+#     x  = int(x)
 
-
-# print(chain["puts"].head())
+tdata.to_csv('data/' + stock + 'calls.csv')
+tdata1.to_csv('data/' + stock + 'puts.csv')
+# # # print(chain["puts"].head())
