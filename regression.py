7a63ce735405dfb59a5154c9f90c3efb74728941
Siddhant Dubey <sid12.dubey@gmail.com>
2020-02-25 21:28:58 -0500

    Modified files to use input from user to get stock data and then plot

--- regression.py
+++ regression.py
@@ -12,30 +12,36 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
+stock = input("Input Stock Ticker: ")
 xs_list = []
 ys_list = []
 zs_list = []
 
 n = 100
 
-datess = ['03/06/2020','03/13/2020','03/20/2020','03/27/2020','04/03/2020','04/17/2020','05/15/2020','06/19/2020','07/17/2020','08/21/2020','09/18/2020','10/16/2020','01/15/2021','03/19/2021','06/18/2021','09/17/2021','01/21/2022','03/18/2022','06/17/2022']
-f_datess = []
-for i in range(len(datess)):
-    d = datess[i]
-    temp = d[6:] + '-' + d[:2] + '-' + d[3:5]
-    f_datess.append(temp)
-# For each set of style and range settings, plot n random points in the box
-# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
-for j in range(len(f_datess)):
-    for m, zlow, zhigh in [('o', -50, -25), ('^', -30, -5)]:
-        data = pd.read_csv('Data/calls' + f_datess[j] + '.csv')
-        data = data[data['Implied Volatility'] != 0]
-        xs = data['Strike'].tolist()
-        ys = data['Maturity'].tolist()
-        zs = data['Implied Volatility'].tolist()
-        xs_list = xs_list + xs
-        ys_list = ys_list + ys
-        zs_list = zs_list + zs
+# datess = ['03/06/2020','03/13/2020','03/20/2020','03/27/2020','04/03/2020','04/17/2020','05/15/2020','06/19/2020','07/17/2020','08/21/2020','09/18/2020','10/16/2020','01/15/2021','03/19/2021','06/18/2021','09/17/2021','01/21/2022','03/18/2022','06/17/2022']
+# f_datess = []
+# for i in range(len(datess)):
+#     d = datess[i]
+#     temp = d[6:] + '-' + d[:2] + '-' + d[3:5]
+#     f_datess.append(temp)
+# # For each set of style and range settings, plot n random points in the box
+# # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+# for j in range(len(f_datess)):
+#     for m, zlow, zhigh in [('o', -50, -25), ('^', -30, -5)]:
+#         data = pd.read_csv('Data/calls' + f_datess[j] + '.csv')
+#         data = data[data['Implied Volatility'] != 0]
+#         xs = data['Strike'].tolist()
+#         ys = data['Maturity'].tolist()
+#         zs = data['Implied Volatility'].tolist()
+#         xs_list = xs_list + xs
+#         ys_list = ys_list + ys
+#         zs_list = zs_list + zs
+
+data = pd.read_csv('Data/' + stock + "calls.csv")
+xs_list = data['Strike'].tolist()
+ys_list = data['Maturity'].tolist()
+zs_list = data['Implied Volatility'].tolist()
 
 temp = np.column_stack((xs_list, ys_list))
 # regr = MLPRegressor(hidden_layer_sizes=400, activation='relu', max_iter=450) #MLP Performs significantly worse than RandomForest
@@ -88,5 +94,5 @@ ax1.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 # Add a color bar which maps values to colors.
 fig1.colorbar(surf1, shrink=0.5, aspect=5)
 
-plt.savefig('Graphics/FitImages/forestregression.png')
+plt.savefig('Graphics/FitImages/' + stock + '/forestregression.png')
 plt.show()
